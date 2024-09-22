2afa870a0e213e9283cee98478e5651550b0c794
Caroline De Oliveira Cortes <cade-oli@c1r4s4.42porto.com>
2024-09-22 15:49:53 +0100

    C06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cade-oli <cade-oli@42porto.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 14:50:20 by cade-oli          #+#    #+#             */
+/*   Updated: 2024/09/22 15:48:30 by cade-oli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = -1;
+	while (s1[++i] != '\0' || s2[i] != '\0')
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	return (0);
+}
+
+void	ft_sort_ascii(int argc, char *argv[])
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - i)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				temp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	ft_sort_ascii(argc, argv);
+	i = 0;
+	while (++i < argc)
+	{
+		write(1, argv[i], ft_strlen(argv[i]));
+		write(1, "\n", 1);
+	}
+	return (42);
+}
